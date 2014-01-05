45df604e1aa71ad0d0999a3460c0db9920706d55
Charles Armitage <code@cjrarmitage.co.uk>
2014-01-05 19:46:07 +0000

    Initial commit of PathCopy

---
+++ PathCopy/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace PathCopy
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length == 0 || args[0] == "--install")
+            {
+                InstallShellExtension();
+            }
+            else if(args[0] == "--uninstall")
+            {
+                UninstallShellExtension();
+            }
+            else
+            {
+                CopyPath(args[0]);
+            }
+            //Console.ReadLine();
+        }
+
+        private static void InstallShellExtension()
+        {
+            ShellExtension.AddFileShellExtension(GetCurrentExeFileInfo(), "Copy Path");
+            ShellExtension.AddDirectoryShellExtension(GetCurrentExeFileInfo(), "Copy Path");
+        }
+
+        private static void UninstallShellExtension()
+        {
+            ShellExtension.RemoveFileShellExtension(GetCurrentExeFileInfo());
+            ShellExtension.RemoveDirectoryShellExtension(GetCurrentExeFileInfo());
+        }
+
+        private static void CopyPath(string path)
+        {
+            System.Windows.Forms.Clipboard.SetText(path);
+        }
+
+        private static FileInfo GetCurrentExeFileInfo()
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var currentExe = Path.Combine(currentDirectory, AppDomain.CurrentDomain.FriendlyName);
+            var fileInfo = new FileInfo(currentExe);
+            return fileInfo;
+        }
+    }
+}
---
+++ PathCopy/ShellExtension.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using Microsoft.Win32;
+
+namespace PathCopy
+{
+    class ShellExtension
+    {
+        public static void AddDirectoryShellExtension(FileInfo executable, string commandText)
+        {
+            const string directoryExtensionKey = @"Directory\shell\";
+            AddShellExtensionToRegistry(directoryExtensionKey, executable, commandText);
+        }
+
+        public static void AddFileShellExtension(FileInfo executable, string commandText)
+        {
+            const string fileExtensionKey = @"*\shell\";
+            AddShellExtensionToRegistry(fileExtensionKey, executable, commandText);
+        }
+
+        public static void RemoveFileShellExtension(FileInfo executable)
+        {
+            const string fileExtensionKey = @"*\shell\";
+            RemoveShellExtensionFromRegistry(fileExtensionKey, executable);
+        }
+
+        public static void RemoveDirectoryShellExtension(FileInfo executable)
+        {
+            const string directoryExtensionKey = @"Directory\shell\";
+            RemoveShellExtensionFromRegistry(directoryExtensionKey, executable);
+        }
+
+        private static void AddShellExtensionToRegistry(string extensionRootKey, FileInfo executable, string commandText)
+        {
+            var executableName = Path.GetFileNameWithoutExtension(executable.Name);
+            var commandNameKey = extensionRootKey + executableName;
+            AddKeyValuePair(commandNameKey, commandText);
+
+            var commandSubKey = commandNameKey + @"\command";
+            var commandValue = executable + @" %1";
+            AddKeyValuePair(commandSubKey, commandValue);
+        }
+
+        private static void RemoveShellExtensionFromRegistry(string extensionRootKey, FileInfo executable)
+        {
+            var executableName = Path.GetFileNameWithoutExtension(executable.Name);
+            var commandNameKey = extensionRootKey + executableName;
+
+            Console.WriteLine("Removing Key:{0}", commandNameKey);
+            Registry.ClassesRoot.DeleteSubKeyTree(commandNameKey);
+        }
+
+        private static void AddKeyValuePair(string key, string value)
+        {
+            Console.WriteLine("Adding Key:{0} Value:{1}", key, value);
+
+            using (var keyValuePair = Registry.ClassesRoot.CreateSubKey(key))
+            {
+                if (keyValuePair != null)
+                {
+                    keyValuePair.SetValue(null, value);
+                }
+            }
+        }
+    }
+}
